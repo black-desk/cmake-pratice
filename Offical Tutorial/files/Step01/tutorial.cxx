a91f20e2906b77e50eeed0ec82655244aabf9362
black_desk <clx814727823@gmail.com>
2021-08-13 13:19:17 +0800

    Offical Tutorial Step01: EXTRA use commit hash as VERSION_PATCH

--- Offical Tutorial/files/Step01/tutorial.cxx
+++ Offical Tutorial/files/Step01/tutorial.cxx
@@ -10,7 +10,8 @@ int main(int argc, char *argv[]) {
   if (argc < 2) {
     // report version
     std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
+              << Tutorial_VERSION_MINOR << "-" << Tutorial_VERSION_PATCH
+              << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
