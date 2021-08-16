63487a9845d714a97895e6797d7eb1f75443ab49
black_desk <clx814727823@gmail.com>
2021-08-16 13:02:28 +0800

    How to create a ProjectConfig.cmake file: start

--- How to create a ProjectConfig.cmake file/files/bar/bar.c
+++ How to create a ProjectConfig.cmake file/files/bar/bar.c
@@ -1,8 +1,7 @@
 
 #include "foo/foo.h"
 
-int main(int argc, char* argv[])
-{
-	foo();
-	return 0;
+int main(int argc, char *argv[]) {
+  foo();
+  return 0;
 }
--- How to create a ProjectConfig.cmake file/files/foo/foo.c
+++ How to create a ProjectConfig.cmake file/files/foo/foo.c
@@ -3,7 +3,4 @@
 
 #include <stdio.h>
 
-void foo(void)
-{
-	printf("This is foo version %s\n", FOOBAR_VERSION_STRING);
-}
+void foo(void) { printf("This is foo version %s\n", FOOBAR_VERSION_STRING); }
